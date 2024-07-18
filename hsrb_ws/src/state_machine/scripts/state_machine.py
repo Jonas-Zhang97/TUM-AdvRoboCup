9cc45d32c448907afc05e5475a4df5943a6a65f1
Runcong Wang <runcong.wang1@gmail.com>
2024-07-18 07:23:36 +0200

    add prototype of statemachine

---
+++ hsrb_ws/src/state_machine/scripts/state_machine.py
@@ -0,0 +1,149 @@
+import rospy
+import smach
+import subprocess
+
+from dataclasses import dataclass, field
+
+from typing import List
+
+
+# class stateDescription:
+@dataclass
+class stateDescription:
+    """
+    struct stateDescription{
+        string stateName;
+        list<string> preRequisite;
+        list<string> postRequisite;
+        }
+    """
+    stateName: str
+    preRequisite: List[str] = field(default_factory=list)
+    postRequisite: List[str] = field(default_factory=list)
+
+
+
+
+
+
+# class for generating states
+class stateMachineGenerator(smach.State):
+    """
+     Constructor for the state machine generator
+     :param pre_requisite: list of pre_requisite states for state matching
+     :param post_requisite: list of post_requisite states for state matching
+     """
+    def __init__(self,stateDescription):
+        self.lpreRequsite = stateDescription.preRequisite
+        self.lpostRequsite = stateDescription.preRequisite
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'])
+
+    def check_pre_requisite(self):
+
+        return
+
+    def execute(self):
+        command = ["ros run", "pkg", "node.py"]
+        process = subprocess.call(command)
+        if process == 0:
+            return 'succeeded'
+        else:
+            return 'failed'
+
+
+
+def stateOdering(stateList):
+    """
+    Function to order the states in a state machine
+    :param stateList: list of stateDescriptions
+    :return: list of ordered stateDescriptions
+    """
+    stateList_ordered = []
+    return stateList_ordered
+
+
+
+
+
+
+
+def main():
+    rospy.init_node('state_machine')
+    sm = smach.StateMachine(outcomes=['overall succeeded', 'overall failed'])
+    # create state descriptions
+    sD_A = stateDescription("stateA",
+                           ["a", "b", "c"],
+                           ["d", "e", "f"])
+    sD_B = stateDescription("stateB",
+                           ["a", "b", "c"],
+                           ["d", "e", "f"])
+    sD_C = stateDescription("stateC",
+                           ["a", "b", "c"],
+                           ["d", "e", "f"])
+    sD_D = stateDescription("stateD",
+                           ["a", "b", "c"],
+                           ["d", "e", "f"])
+    sD_E = stateDescription("stateE",
+                           ["a", "b", "c"],
+                           ["d", "e", "f"])
+
+    # create instances of stateMachineGenerator
+    STATE_A = stateMachineGenerator(sD_A)
+    STATE_B = stateMachineGenerator(sD_B)
+    STATE_C = stateMachineGenerator(sD_C)
+    STATE_D = stateMachineGenerator(sD_D)
+    STATE_E = stateMachineGenerator(sD_E)
+
+    state_list = [sD_A, sD_B, sD_C, sD_D, sD_E]
+
+    with sm:
+        state_list_ordered = stateOdering(state_list)
+        for state in state_list_ordered:
+            smach.StateMachine.add(state.stateName, stateMachineGenerator(state),
+                                   transitions={'succeeded': 'succeeded',
+                                                'failed': 'failed'})
+
+    outcome = sm.execute()
+    rospy.spin()
+    if outcome == 'overall succeeded':
+        print("Overall succeeded")
+    else:
+        print("Overall failed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    stateA = stateDescription("stateA",
+                           ["a", "b", "c"],
+                           ["d", "e", "f"])
+    print('stateA',stateA)
+    print('stateA.stateName',stateA.stateName)
+    print('stateA.preRequisite',stateA.preRequisite)
+    print('stateA.postRequisite',stateA.postRequisite)
+
+    stateB = stateDescription("stateB",
+                              ["a", "b", "c"],
+                                ["d", "e", "f"])
+
+
+    test = [stateA, stateB]
+    print('test',test)
+    print('test[0]',test[0])
+    print('test[1]',test[1])
+
