bc698a15f5079f495cf29973147c34860076a711
gugu in nest <jonas.zhang97@gmail.com>
2024-07-23 00:37:59 +0200

    update

---
+++ hsrb_ws/src/pick_place_action/include/pick_place/place.h
@@ -0,0 +1,87 @@
+#ifndef PLACE_H
+#define PLACE_H
+
+#include <ros/ros.h>
+
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <tmc_control_msgs/GripperApplyEffortActionGoal.h>
+
+#include <std_msgs/Bool.h>
+
+class Place
+{
+  private:
+    moveit::planning_interface::MoveGroupInterface whole_body_grp;
+    moveit::planning_interface::MoveGroupInterface arm_grp;
+    moveit::planning_interface::MoveGroupInterface gripper_grp;
+
+  public:
+    Place():whole_body_grp("whole_body"), arm_grp("arm"), gripper_grp("gripper") {};
+
+  public:
+    bool init();
+    void update();
+
+  /* Robot Motion */
+  public:
+    void place();
+  
+  private:
+    void computeTargetOrientation();
+    void prePlaceApproach();
+    void toPlacePose();
+    void openGripper();
+    void postPlaceRetreat();
+    void homing();
+
+  /* ROS Communication */
+  public:
+    ros::NodeHandle nh_;
+
+  private:
+    ros::Subscriber place_target_sub_;   // where is the target object, store this in a local variable
+
+    ros::Publisher gripper_pub_;
+    ros::Publisher place_done_pub_;
+
+  public:
+    std::string place_target_topic_;
+
+    std::string place_done_topic_;
+
+  private:
+    void poseCallback(const geometry_msgs::PoseStamped::ConstPtr &msg);
+
+  /* Local Variables */
+  private:
+    // for ROS
+    bool command_;
+    bool lower_torso_;
+    std_msgs::Bool place_done_;
+
+    int counter_;         // After 3 place processes, higher the torso for the pick on the high table
+
+    // some assistance vars
+    std::string ref_frame_;
+    geometry_msgs::Point target_position_;
+    double target_orientation_;
+
+    std::vector<double> arm_home_value_;
+    // std::vector<double> gripper_open_value_;
+    trajectory_msgs::JointTrajectory gripper_close_value_;
+
+    trajectory_msgs::JointTrajectory gripper_open_value_;
+
+  /* MoveIt */
+  private:
+    moveit::planning_interface::MoveGroupInterface::Plan whole_body_plan_;
+    moveit::planning_interface::MoveGroupInterface::Plan arm_plan_;
+    moveit::planning_interface::MoveGroupInterface::Plan gripper_plan_;
+    moveit::planning_interface::PlanningSceneInterface PSI_;
+};
+
+#endif
---
+++ hsrb_ws/src/pick_place_action/src/pick.cpp
@@ -0,0 +1,222 @@
+#include <pick_place/pick.h>
+
+// bool Pick::init()
+// {
+//   // Topic definitions
+//   pick_target_topic_ =  "/labeled_objects";
+//   pick_done_topic_ = "/pick_done";
+// 
+//   // Subscribers and publishers
+//   pick_target_sub_ = nh_.subscribe(pick_target_topic_, 1, &Pick::poseCallback, this);
+// 
+//   gripper_pub_ = nh_.advertise<tmc_control_msgs::GripperApplyEffortActionGoal>("/hsrb/gripper_controller/apply_force/goal", 10);
+//   pick_done_pub_ = nh_.advertise<std_msgs::Bool>(pick_done_topic_, 1);
+// 
+//   // Initialize flags
+//   command_ = false;
+//   pick_done_.data = true;
+//   
+//   // Pre-defined poses
+//   transport_value_ = {0.0, 0.0, -1.57, -1.57, 0.0, 0.0};
+//   gripper_open_value_ = {0.8};
+//   
+//   // Set values for MoveIt
+//   ref_frame_ = "odom";
+// 
+//   whole_body_grp.setPlannerId("RRTConnectkConfigDefault");
+//   whole_body_grp.setPlanningTime(30.0);
+//   whole_body_grp.setMaxAccelerationScalingFactor(0.1);
+//   whole_body_grp.setMaxVelocityScalingFactor(0.1);
+//   whole_body_grp.allowReplanning(true);
+//   whole_body_grp.setNumPlanningAttempts(10);
+//   // whole_body_grp.setEndEffectorLink("hand_camera_frame");
+//   const std::string end_effector_link = whole_body_grp.getEndEffectorLink();
+// 
+//   // Initialize HSRB
+//   // openGripper();
+//   toTransportPose();
+//   ROS_INFO_STREAM("Pick Node Initialized!");
+//   return true;
+// }
+
+void Pick::executeCB(const pick_place::PickGoalConstPtr &goal)
+{
+  ros::Duration(2.0).sleep();
+
+  gripper_pub_ = nh_.advertise<tmc_control_msgs::GripperApplyEffortActionGoal>("/hsrb/gripper_controller/apply_force/goal", 10);
+  
+  // Perform pick
+  pick(goal);
+
+  // Publish msgs that are needed
+  // pick_done_pub_.publish(pick_done_);
+
+  // Clear all collision objects in the planning scene
+  object_names_ = PSI_.getKnownObjectNames();
+
+  ROS_INFO_STREAM("Number of collision objects in the scene: " << object_names_.size());
+    
+  PSI_.removeCollisionObjects(object_names_);
+  object_names_.clear();
+  pick_res_.completed_flag = true;
+}
+
+void Pick::pick(const pick_place::PickGoalConstPtr &goal)
+{
+  computeTargetOrientation(goal);
+  prePickApproach();
+  openGripper();
+  toPickPose();
+  closeGripper();
+  postPickRetreat();
+  toTransportPose();
+}
+
+void Pick::computeTargetOrientation(const pick_place::PickGoalConstPtr &goal)
+{
+  geometry_msgs::Point current_base_position;
+  target_position_ = goal -> target_pose;
+  current_base_position = whole_body_grp.getCurrentPose("base_link").pose.position;
+  geometry_msgs::Vector3 vec;
+  vec.x = target_position_.x - current_base_position.x;
+  vec.y = target_position_.y - current_base_position.y;
+  vec.z = target_position_.z - current_base_position.z;
+
+  target_orientation_ = atan2(vec.y, vec.x);
+}
+
+void Pick::prePickApproach()
+{
+  /* 
+    A pre approach for the robot, will be followe by a straight line motion to the pick pose
+  */
+  
+  // define pre-approach pose
+  geometry_msgs::PoseStamped pre_approach_pose;
+  pre_approach_pose.header.frame_id = ref_frame_;
+  pre_approach_pose.pose.position.x = target_position_.x - 0.18 * cos(target_orientation_);
+  pre_approach_pose.pose.position.y = target_position_.y - 0.18 * sin(target_orientation_);
+  pre_approach_pose.pose.position.z = target_position_.z;
+
+  tf2::Quaternion quaternion;
+  quaternion.setRPY(-1.57, -1.57, target_orientation_ - 1.57);
+
+  pre_approach_pose.pose.orientation = tf2::toMsg(quaternion);
+
+  ROS_INFO_STREAM("Pre-approach pose: \n" << pre_approach_pose.pose);
+
+  whole_body_grp.setPoseTarget(pre_approach_pose);
+  bool succ = (whole_body_grp.plan(body_plan_) == moveit_msgs::MoveItErrorCodes::SUCCESS);
+
+  if (!succ)
+  {
+    ROS_INFO_STREAM("Planning failed");
+  }
+
+  whole_body_grp.move();
+  ROS_INFO_STREAM("Pre-pick goal reached");
+  whole_body_grp.clearPathConstraints();
+}
+// 
+void Pick::openGripper()     // TODO: Implement this
+{
+  tmc_control_msgs::GripperApplyEffortActionGoal open_goal;
+  open_goal.goal.effort = -1;
+  open_goal.goal.do_control_stop = true;
+  gripper_pub_.publish(open_goal);
+  ros::Duration(0.5).sleep();
+}
+// 
+void Pick::toPickPose()
+{
+  geometry_msgs::Pose pick_pose;
+  pick_pose.position.x = target_position_.x - 0.03 * cos(target_orientation_);
+  pick_pose.position.y = target_position_.y - 0.03 * sin(target_orientation_);
+  pick_pose.position.z = target_position_.z;
+
+  tf2::Quaternion quaternion;
+  quaternion.setRPY(-1.57, -1.57, target_orientation_ - 1.57);
+  pick_pose.orientation = tf2::toMsg(quaternion);
+  ROS_INFO_STREAM("Pick pose: " << pick_pose);
+
+  std::vector<geometry_msgs::Pose> waypoints;
+  waypoints.push_back(pick_pose);
+
+  moveit_msgs::RobotTrajectory trajectory;
+
+  double eef_step = 0.01;  // Resolution of the Cartesian path
+  double jump_threshold = 0.0;  // No jump threshold
+
+  double fraction = whole_body_grp.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+
+  ROS_INFO_STREAM("Straight line generation rate = " << fraction);
+
+  whole_body_grp.execute(trajectory);
+
+  ROS_INFO_STREAM("Arrived at: " << pick_pose.position);
+}
+// 
+void Pick::closeGripper()
+{
+  tmc_control_msgs::GripperApplyEffortActionGoal open_goal;
+  open_goal.goal.effort = 5;
+  gripper_pub_.publish(open_goal);
+  ros::Duration(0.5).sleep();
+}
+//
+// 
+void Pick::postPickRetreat()
+{
+  geometry_msgs::Pose post_pick_pose_1;
+  post_pick_pose_1.position.x = target_position_.x - 0.03 * cos(target_orientation_);
+  post_pick_pose_1.position.y = target_position_.y - 0.03 * sin(target_orientation_);
+  post_pick_pose_1.position.z = target_position_.z + 0.1;
+
+  geometry_msgs::Pose post_pick_pose_2;
+  post_pick_pose_2.position.x = target_position_.x - 0.21 * cos(target_orientation_);
+  post_pick_pose_2.position.y = target_position_.y - 0.21 * sin(target_orientation_);
+  post_pick_pose_2.position.z = target_position_.z + 0.1;
+
+  tf2::Quaternion quaternion;
+  quaternion.setRPY(-1.57, -1.57, target_orientation_ - 1.57);
+  post_pick_pose_1.orientation = tf2::toMsg(quaternion);
+  post_pick_pose_2.orientation = tf2::toMsg(quaternion);
+
+  std::vector<geometry_msgs::Pose> waypoints;
+  waypoints.push_back(post_pick_pose_1);
+  waypoints.push_back(post_pick_pose_2);
+
+  moveit_msgs::RobotTrajectory trajectory;
+
+  double eef_step = 0.01;  // Resolution of the Cartesian path
+  double jump_threshold = 0.0;  // No jump threshold
+
+  double fraction = whole_body_grp.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+
+  whole_body_grp.execute(trajectory);
+
+  ROS_INFO_STREAM("Retreated");
+}
+
+void Pick::toTransportPose()
+{
+  arm_grp.setJointValueTarget(transport_value_);
+  arm_grp.plan(arm_plan_);
+  arm_grp.move();
+
+  ROS_INFO_STREAM("Ready to go");
+}
+
+
+void Pick::poseCallback(const project_msgs::LabeledCentroid::ConstPtr &msg)
+{
+  labeled_centroid_ = *msg;
+
+  command_ = true;
+
+  object_label_ = msg->label;
+
+  target_position_ = msg->pose.pose.position;
+
+  ROS_INFO_STREAM("Command received, position: " << target_position_);
+}
